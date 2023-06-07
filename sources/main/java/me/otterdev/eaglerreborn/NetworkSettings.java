59f9210134d8cf015f671e81b322b5ca07d29028
OtterDev <134523836+OtterCodes101@users.noreply.github.com>
2023-06-07 09:53:35 -0600

    add default relay

--- sources/main/java/me/otterdev/eaglerreborn/NetworkSettings.java
+++ sources/main/java/me/otterdev/eaglerreborn/NetworkSettings.java
@@ -4,4 +4,5 @@ public class NetworkSettings {
   //this doesnt work rn so whatever u do dont set it to no
   
   public static final String legacyMode = "yes";
+  public static final String defaultRelay = "wss://reborn-relay.otterdev.repl.co";
 }
