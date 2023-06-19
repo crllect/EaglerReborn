127cb11b61453db1f45eb018f30b6a0cdf3e6818
OtterDev <134523836+OtterCodes101@users.noreply.github.com>
2023-06-19 11:18:59 -0600

    WIP TCP API
    chances are, your chromebook doesn't support this.
    you must have an old chromebook that has been out of date for over a year.
    Sorry!

---
+++ sources/main/java/me/otterdev/eaglerreborn/TCPAPI.java
@@ -0,0 +1,27 @@
+package me.otterdev.eaglerreborn;
+
+import java.util.LinkedList;
+import java.util.List; 
+
+import net.lax1dude.eaglercraft.v1_8.internal.teavm.TeaVMUtils;
+import org.teavm.jso.JSBody;
+import org.teavm.jso.JSObject;
+import org.teavm.jso.browser.TimerHandler;
+import org.teavm.jso.browser.Window;
+import org.teavm.jso.dom.events.Event;
+import org.teavm.jso.dom.events.EventListener;
+import org.teavm.jso.dom.events.KeyboardEvent;
+import org.teavm.jso.dom.events.MouseEvent;
+import org.teavm.jso.dom.events.WheelEvent;
+import org.teavm.jso.dom.html.HTMLCanvasElement;
+import org.teavm.jso.dom.html.HTMLElement;
+import org.teavm.jso.webgl.WebGLFramebuffer;
+import org.teavm.jso.webgl.WebGLRenderbuffer;
+
+import net.lax1dude.eaglercraft.v1_8.EagUtils;
+
+public class TCPAPI {
+
+  @JSBody(params = { }, script = "if(window.chrome && chrome.app && chrome.app.runtime){return true;}else{return false;}")
+  public static native boolean betterTCPSupported();
+}
