efc9368da2beef50f68696cc947fbdecf7a84a4d
OtterDev <134523836+OtterCodes101@users.noreply.github.com>
2023-08-16 08:49:06 -0600

    Begin testing for custom relay via EaglerXOpts

--- sources/main/java/me/otterdev/eaglerreborn/NetworkSettings.java
+++ sources/main/java/me/otterdev/eaglerreborn/NetworkSettings.java
@@ -1,7 +1,14 @@
 package me.otterdev.eaglerreborn;
+
+import org.teavm.jso.JSBody;
+import org.json.JSONObject;
+
 public class NetworkSettings {
-  // change this to ur relay
-  //                                          |
-  //                                          \/
-  public static final String defaultRelay = "wss://reborn-relay.otterdev.repl.co";
+  @JSBody(params = {}, script = "if(typeof window.eaglercraftXOpts === \"undefined\") {return null;}"
+			+ "else if(typeof window.eaglercraftXOpts === \"string\") {return window.eaglercraftXOpts;}"
+			+ "else {return JSON.stringify(window.eaglercraftXOpts);}")
+	private static native String getEaglerXOpts();
+  String opts = getEaglerXOpts();
+  JSONObject eaglerOpts = new JSONObject(opts);
+  public static final String defaultRelay = eaglerOpts.getString("relayURL");
 }
