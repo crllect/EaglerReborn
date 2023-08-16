412fc07972c9a912dd23821fa9d5a27a8ea10776
OtterDev <134523836+OtterCodes101@users.noreply.github.com>
2023-08-16 15:17:32 -0600

    Update NetworkSettings.java

--- sources/main/java/me/otterdev/eaglerreborn/NetworkSettings.java
+++ sources/main/java/me/otterdev/eaglerreborn/NetworkSettings.java
@@ -1,14 +1,5 @@
 package me.otterdev.eaglerreborn;
 
-import org.teavm.jso.JSBody;
-import org.json.JSONObject;
-
 public class NetworkSettings {
-  @JSBody(params = {}, script = "if(typeof window.eaglercraftXOpts === \"undefined\") {return null;}"
-			+ "else if(typeof window.eaglercraftXOpts === \"string\") {return window.eaglercraftXOpts;}"
-			+ "else {return JSON.stringify(window.eaglercraftXOpts);}")
-	private static native String getEaglerXOpts();
-  String opts = getEaglerXOpts();
-  JSONObject eaglerOpts = new JSONObject(opts);
-  public static final String defaultRelay = eaglerOpts.getString("relayURL");
+  public static final String defaultRelay = "balls";
 }
