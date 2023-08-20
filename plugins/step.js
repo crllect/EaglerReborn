bea944d749506187ddb3bb656aefe16bee672f00
ZXMushroom63 <116805577+ZXMushroom63@users.noreply.github.com>
2023-08-20 17:27:44 +0800

    Create step.js

---
+++ plugins/step.js
@@ -0,0 +1,4 @@
+PluginAPI.addEventListener("update", ()=>{ //Every client tick, allow the player to step up over up to nine and a half blocks.
+  PluginAPI.player.stepHeight = 9.5;
+  PluginAPI.updateComponent("player");
+});
